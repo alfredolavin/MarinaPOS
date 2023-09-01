1ae2e0acaacd5bb2dd8dc21ca83072beafd8b01f
alfredolavin <alfredolavin@gmail.com>
2023-09-01 00:17:30 -0400

    Borrando objetos innecesarios

--- antiguos/mySqlWorkbench/trigger_2.sql
+++
@@ -1,6 +0,0 @@
-CREATE TRIGGER a_mayusculas
-	BEFORE UPDATE ON `usuario`
-		FOR EACH ROW SET
-			NEW.nombre = UPPER(NEW.nombre),
-			NEW.apellido = UPPER(NEW.apellido),
-			NEW.email = UPPER(NEW.email);

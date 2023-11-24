c824a55cd921101ec0a23ad53767683c7b72774e
alfredolavin <alfredolavin@gmail.com>
2023-11-24 00:29:03 -0300

    Reacomodando archivos

--- db/modelo/MarinaPOS.sql
+++ db/modelo/MarinaPOS.sql
@@ -4,37 +4,33 @@ PRAGMA writable_schema = 0;
 
 CREATE TABLE producto(
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-  nombre_largo TEXT NOT NULL,
   nombre TEXT NOT NULL,
-  concentracion TEXT NOT NULL,
-  unidad_de_concentracion INTEGER NOT NULL,
   cantidad INTEGER NOT NULL,
-  unidad_de_cantidad INTEGER NOT NULL,
-  laboratorio_id INTEGER NOT NULL,
   forma_farmaceutica_id INTEGER NOT NULL,
+  laboratorio_id INTEGER NOT NULL,
   es_generico BOOLEAN,
   es_bioequivalente BOOLEAN,
   es_refrigerado BOOLEAN,
   es_controlado BOOLEAN,
-  es_activo BOOLEAN,
-  es_petitorio_minimo BOOLEAN,
+  es_desactivado BOOLEAN,
+  es_cenabast BOOLEAN,
   CONSTRAINT laboratorio_producto FOREIGN KEY (laboratorio_id) REFERENCES laboratorio (id),
-  CONSTRAINT forma_farmaceutica_producto FOREIGN KEY (forma_farmaceutica_id) REFERENCES forma_farmaceutica (id),
-  CONSTRAINT unidad_de_medida_producto FOREIGN KEY (unidad_de_cantidad) REFERENCES unidad_de_medida (unidad_de_medida_id),
-  CONSTRAINT unidad_de_medida_producto
-    FOREIGN KEY (unidad_de_concentracion) REFERENCES unidad_de_medida (unidad_de_medida_id)
+  CONSTRAINT forma_farmaceutica_producto FOREIGN KEY (forma_farmaceutica_id) REFERENCES forma_farmaceutica (id)
 );
 
-  CREATE INDEX productos_ix_1 ON producto(nombre_largo);
+  CREATE INDEX productos_ix_1 ON producto;
   
 CREATE TABLE codigo_de_barras(
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  barras TEXT NOT NULL,
+  id TEXT NOT NULL,
   producto_id INTEGER NOT NULL,
-  CONSTRAINT productos_codigos_de_barra FOREIGN KEY (producto_id) REFERENCES producto (id)
+  tipo_detalle_id INTEGER NOT NULL,
+  detalle TEXT,
+  PRIMARY KEY(id),
+  CONSTRAINT productos_codigos_de_barra FOREIGN KEY (producto_id) REFERENCES producto (id),
+  CONSTRAINT tipo_detalle_codigo_de_barras FOREIGN KEY (tipo_detalle_id) REFERENCES tipo_detalle (id)
 );
 
-  CREATE UNIQUE INDEX codigo_de_barras_ix_1 ON codigo_de_barras(barras);
+  CREATE UNIQUE INDEX codigo_de_barras_ix_1 ON codigo_de_barras(id);
   
 CREATE TABLE documento(
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -123,36 +119,22 @@ CREATE TABLE principio_activo(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nom
 CREATE TABLE principio_activo_y_producto(
   producto_id INTEGER NOT NULL,
   principio_activo_id INTEGER NOT NULL,
-  unidad_de_medida_id INTEGER NOT NULL,
-  concentracion INTEGER NOT NULL,
   PRIMARY KEY(producto_id, principio_activo_id),
   CONSTRAINT producto_table1 FOREIGN KEY (producto_id) REFERENCES producto (id),
-  CONSTRAINT principio_activo_table1 FOREIGN KEY (principio_activo_id) REFERENCES principio_activo (id),
-  CONSTRAINT unidad_de_medida_table1 FOREIGN KEY (unidad_de_medida_id) REFERENCES unidad_de_medida (unidad_de_medida_id)
+  CONSTRAINT principio_activo_table1 FOREIGN KEY (principio_activo_id) REFERENCES principio_activo (id)
 );
 
 CREATE TABLE tipo_detalle(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, nombre_detalle TEXT NOT NULL);
 
-CREATE TABLE detalle_producto(
-  producto_id INTEGER NOT NULL,
-  tipo_detalles_id INTEGER NOT NULL,
-  detalle TEXT NOT NULL,
-  PRIMARY KEY(producto_id, tipo_detalles_id),
-  CONSTRAINT producto_detalles_producto FOREIGN KEY (producto_id) REFERENCES producto (id),
-  CONSTRAINT tipo_detalles_detalles_producto FOREIGN KEY (tipo_detalles_id) REFERENCES tipo_detalle (id)
-);
-
-  CREATE UNIQUE INDEX detalles_producto_ix_1 ON detalle_producto(producto_id, tipo_detalles_id);
-  
 CREATE TABLE producto_precio(
   id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
   producto_id INTEGER NOT NULL,
-  usuario_id INTEGER NOT NULL,
   momento DATETIME NOT NULL,
   precio_venta INTEGER NOT NULL,
   precio_compra INTEGER,
+  turno_id INTEGER NOT NULL,
   CONSTRAINT producto_producto_precio FOREIGN KEY (producto_id) REFERENCES producto (id),
-  CONSTRAINT usuario_producto_precio FOREIGN KEY (usuario_id) REFERENCES usuario (id)
+  CONSTRAINT turno_producto_precio FOREIGN KEY (turno_id) REFERENCES turno (id)
 );
 
 CREATE TABLE principio_activo_y_codigo_atc(
@@ -162,4 +144,15 @@ CREATE TABLE principio_activo_y_codigo_atc(
   CONSTRAINT principio_activo_principio_activo_y_codigo_atc
     FOREIGN KEY (principio_activo_id) REFERENCES principio_activo (id),
   CONSTRAINT codigo_atc_principio_activo_y_codigo_atc FOREIGN KEY (codigo_atc_id) REFERENCES codigo_atc (id)
+);
+
+CREATE TABLE detalles_farmaceuticos(
+  id INTEGER NOT NULL,
+  cenabast❓ BOOLEAN,
+  controlado❓ BOOLEAN,
+  refrigerado❓ BOOLEAN,
+  bioequivalente❓ BOOLEAN,
+  generico❓ BOOLEAN,
+  PRIMARY KEY(id),
+  CONSTRAINT producto_detalles_farmaceuticos FOREIGN KEY (id) REFERENCES producto (id)
 );
